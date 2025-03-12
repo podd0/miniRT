575b9a445f227c4c2cca9041d74931341293bd7f
Ani Mema <amema@e3r9p1.42roma.it>
2025-03-12 14:16:31 +0100

    fix: norm fix in light_transform.c

--- light_transform.c
+++ light_transform.c
@@ -6,7 +6,7 @@
 /*   By: amema <amema@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 12:01:42 by amema             #+#    #+#             */
-/*   Updated: 2025/03/12 12:16:08 by amema            ###   ########.fr       */
+/*   Updated: 2025/03/12 14:15:36 by amema            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,19 @@ void	translate_light(t_light *light, t_vec3 delta)
 	light->pos = add(light->pos, delta);
 }
 
-// approach1: rotate a light around the world origin (0, 0, 0)
-void rotate_light(t_light *light, t_frame rot)
+void	rotate_light(t_light *light, t_frame rot)
 {
-	t_vec3 new_pos = v_to_world(light->pos, rot);
+	t_vec3	new_pos;
+
+	new_pos = v_to_world(light->pos, rot);
 	light->pos = new_pos;
 }
 
-/*
-** approach2: rotate a light around a pivot point
-** This function rotates a light around a given pivot point using a specified
-** rotation.
-** Example: light (5, 0, 0) and pivot (0, 0, 0) rotated by 90 degrees around the
-** z-axis will move the light to (0, 5, 0).
-*/
 void	rotate_light_pivot(t_light *light, t_frame rot, t_vec3 pivot)
 {
-	t_vec3 rel = sub(light->pos, pivot); // calc relative vector pivot -> light
-	rel = v_to_world(rel, rot); // rotare relative vector
-	light->pos = add(pivot, rel);		 // update light position by adding the pivot to the rotated vector
+	t_vec3	rel;
+
+	rel = sub(light->pos, pivot);
+	rel = v_to_world(rel, rot);
+	light->pos = add(pivot, rel);
 }
